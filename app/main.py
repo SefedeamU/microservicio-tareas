aec435e79c404e36a5d45d0fde70bba56192503a
Sefedeam <sergio.delgado.a@utec.edu.pe>
2024-10-07 00:00:35 -0500

    v1.6

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from app.api import tasks, comments
 from app.db import models
 from app.db.session import engine
-
+import uvicorn
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
@@ -17,3 +17,6 @@ async def root():
 @app.get("/hello/{name}")
 async def say_hello(name: str):
     return {"message": f"Hello {name}"}
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8002)
